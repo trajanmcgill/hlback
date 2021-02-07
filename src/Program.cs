8a3ba6f55068acfdb224dc2555d54e66b97e550d
Trajan McGill <code@trajanmcgill.com>
2021-02-07 14:28:34 -0600

    Added basic usage instructions.
    Issue: #21

--- src/Program.cs
+++ src/Program.cs
@@ -22,6 +22,7 @@ namespace hlback
 			try { config = OptionsProcessor.getRuntimeConfiguration(args); }
 			catch (ErrorManagement.OptionsException e)
 			{
+				userInterface.report("", ConsoleOutput.Verbosity.ErrorsAndWarnings);
 				userInterface.report($"Error: {e.Message}", ConsoleOutput.Verbosity.ErrorsAndWarnings);
 				printUsage(userInterface);
 				return (int)ExitCode.Error_Usage;
@@ -37,7 +38,16 @@ namespace hlback
 
 		private static void printUsage(ConsoleOutput userInterface)
 		{
-			userInterface.report("Usage: ADD USAGE DESCRIPTION", ConsoleOutput.Verbosity.ErrorsAndWarnings); // CHANGE CODE HERE
+			userInterface.report("", ConsoleOutput.Verbosity.ErrorsAndWarnings);
+			userInterface.report("Usage: hlback [--MaxHardLinkAge AGE] [--MaxHardLinksPerFile LINKSCOUNT] SOURCE DESTINATION", ConsoleOutput.Verbosity.ErrorsAndWarnings);
+			userInterface.report("", ConsoleOutput.Verbosity.ErrorsAndWarnings);
+			userInterface.report("Backs up files from SOURCE path to a time-stamped directory in DESTINATION path.", ConsoleOutput.Verbosity.ErrorsAndWarnings);
+			userInterface.report("", ConsoleOutput.Verbosity.ErrorsAndWarnings);
+			userInterface.report("Where possible, creates hard links to previous backup copies instead of new full copies, subject to the below rules:", ConsoleOutput.Verbosity.ErrorsAndWarnings);
+			userInterface.report(1, "--MaxHardLinkAge or -MA (or, on Windows only, /MaxHardLinkAge or /MA):", ConsoleOutput.Verbosity.ErrorsAndWarnings);
+			userInterface.report(2, "if specified, will limit hard links to targets that are under AGE days old (creates a new full copy if all previous copies are too old).", ConsoleOutput.Verbosity.ErrorsAndWarnings);
+			userInterface.report(1, "--MaxHardLinksPerFile or -ML (or, on Windows only, /MaxHardLinksPerFile or /ML):", ConsoleOutput.Verbosity.ErrorsAndWarnings);
+			userInterface.report(2, "if specified, will limit the number of hard links to a particular physical copy to LINKSCOUNT (creates a new full copy if this number would be exceeded).", ConsoleOutput.Verbosity.ErrorsAndWarnings);
 		} // end printUsage()
 
     } // end class Program
