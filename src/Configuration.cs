909e178064faf4ff799e8db62236ea44e2648cf4
Trajan McGill <code@trajanmcgill.com>
2021-03-06 14:40:01 -0600

    Full documentation added to Configuration class.
    Issue: #3

--- src/Configuration.cs
+++ src/Configuration.cs
@@ -6,18 +6,48 @@ using hlback.FileManagement;
 
 namespace hlback
 {
+	// Configuration:
+	/// <summary>Represents a group of settings that apply to a run of the application.</summary>
 	class Configuration
 	{
+		// SystemType:
+		/// <summary>Enum with values corresponding to possible operating system environments.</summary>
 		public enum SystemType { Linux, Windows, Other };
 
+		/// <summary>
+		/// 	Value indicating the maximum number of files which should be allowed to point to the same actual stored data before making a new physical copy of a file.
+		/// 	If <c>null</c>, indicates the number of hard links per file has no limit.
+		/// </summary>
 		public readonly int? MaxHardLinksPerFile;
+
+		/// <summary>
+		/// 	Value indicating the maximum age (in days) a previous backup copy can attain before new hard links are not made to it and a new physical copy is made instead.
+		/// 	If <c>null</c>, indicates there is no such age limit.
+		/// </summary>
 		public readonly int? MaxDaysBeforeNewFullFileCopy;
+
+		/// <summary>A collection of source paths from which to make backups.</summary>
 		public readonly ReadOnlyCollection<SourcePathInfo> BackupSourcePaths;
+
+		/// <summary>A full path to a base location in which backups should be placed.</summary>
 		public readonly string BackupDestinationPath;
 
 
+		// Configuration constructor:
+		/// <summary>Sets up the new <c>Configuration</c> object.</summary>
+		/// <param name="maxHardLinksPerFile">
+		/// 	Nullable <c>int</c> indicating the maximum number of files which should be allowed to point to the same actual stored data before making a new physical copy of a file.
+		/// 	If <c>null</c>, indicates the number of hard links per file has no limit.
+		/// </param>
+		/// <param name="maxDaysBeforeNewFullFileCopy">
+		/// 	Nullable <c>int</c> indicating the maximum age (in days) a previous backup copy can attain before new hard links are not made to it and a new physical copy is made instead.
+		/// 	If <c>null</c>, indicates there is no such age limit.
+		/// </param>
+		/// <param name="backupSourcePaths">A collection of source paths from which to make backups.</param>
+		/// <param name="backupDestinationPath">A full path to a base location in which backups should be placed.</param>
 		public Configuration(int? maxHardLinksPerFile, int? maxDaysBeforeNewFullFileCopy, List<SourcePathInfo> backupSourcePaths, string backupDestinationPath)
 		{
+			// Initialize member variables.
 			MaxHardLinksPerFile = maxHardLinksPerFile;
 			MaxDaysBeforeNewFullFileCopy = maxDaysBeforeNewFullFileCopy;
 			BackupSourcePaths = new ReadOnlyCollection<SourcePathInfo>(backupSourcePaths);
@@ -25,8 +55,12 @@ namespace hlback
 		} // end Configuration constructor
 
 
+		// getSystemType():
+		/// <summary>Ascertains the current operating system environment.</summary>
+		/// <returns>A <c>Configuration.SystemType</c> enum value corresponding to the operating system platform in use.</returns>
 		public static SystemType getSystemType()
 		{
+			// Recognize Windows or Linux; other platforms not supported at this time.
 			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 				return SystemType.Windows;
 			else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
