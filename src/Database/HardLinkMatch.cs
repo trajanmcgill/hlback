499fc516a79f23bac225a33dff4fda688f13a034
Trajan McGill <code@trajanmcgill.com>
2021-03-12 17:19:30 -0600

    Code documentation added to HardLinkMatch.cs
    Issue: #3

--- src/Database/HardLinkMatch.cs
+++ src/Database/HardLinkMatch.cs
@@ -2,9 +2,14 @@ using System;
 
 namespace hlback.Database
 {
+	// HardLinkMatch:
+	/// <summary>Class encapsulating a file match found in the database.</summary>
 	class HardLinkMatch
 	{
+		/// <summary>ID of the corresponding entry in the LiteDB database.</summary>
 		public LiteDB.ObjectId ID { get; init; }
+
+		/// <summary>Full path of the existing file which is a match.</summary>
 		public string MatchingFilePath { get; init; }
 	}
 }
