99a7fc9dbc0e1d108a6bc4de37eff0b54c3f1de7
Trajan McGill <code@trajanmcgill.com>
2021-03-14 19:14:31 -0500

    Linux hard linker code fixed.
    Fixes: #40
    Fixes: #39

--- src/FileManagement/LinuxLinker.cs
+++ src/FileManagement/LinuxLinker.cs
@@ -13,18 +13,18 @@ namespace hlback.FileManagement
 		/// <param name="existingTargetFileName">A <c>string</c> containing the full name (path + name) of an existing file whose physical data will be linked to.</param>
 		public void createHardLink(string newLinkFileName, string existingTargetFileName)
 		{
-			// In Linux, need to do this by kicking off the cp command.
-			// Set up the info for starting an external process to run cp and pass it the appropriate arguments.
+			// In Linux, need to do this by kicking off the ln command.
+			// Set up the info for starting an external process to run ln and pass it the appropriate arguments.
 			ProcessStartInfo startInfo =
 				new ProcessStartInfo()
 				{
-					FileName = "/bin/cp",
+					FileName = "/bin/ln",
 					ArgumentList = {existingTargetFileName, newLinkFileName},
 					UseShellExecute = false,
 					CreateNoWindow = true
 				};
 
-			// Create the cp command process and wait for it to complete before returning.
+			// Create the ln command process and wait for it to complete before returning.
 			using(Process commandProcess = new Process() { StartInfo = startInfo })
 			{
 				// CHANGE CODE HERE: deal with errors running the process to create links
